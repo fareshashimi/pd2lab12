8dd95b406e543cec527155a3d819b80af331b658
fareshashimi <134107064+fareshashimi@users.noreply.github.com>
2023-09-10 21:13:40 +0300

    Update homeWorkNumTwo.c

--- homeWorkNumTwo.c
+++ homeWorkNumTwo.c
@@ -2,87 +2,84 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STUDENTS 100
+typedef struct node{
+    int student_no;
+    char name[20];
+    char department[20];
+}student;
 
-struct Student {
-    int studentNo;
-    char name[50];
-    char department[50];
-};
+void addStdRecord(FILE *mp){
+    student std;
+    
+    printf("Number: ");
+    scanf("%d", &std.student_no);
+    printf("Name: ");
+    scanf("%s", std.name);
+    printf("Department: ");
+    scanf("%s", std.department);
+    
+    fseek(mp, 0, SEEK_SET);
+    fwrite(&std, sizeof(student), 1, mp);
+    printf("\n\n The record is added successfully.\n\n");
+}
 
-int main() {
-    struct Student students[MAX_STUDENTS];
-    int numStudents = 0;
+void searchFileByName(FILE *mp){
+    int stdNum;
+    printf("Insert a student number to search:\n");
+    scanf("%d", &stdNum);
+    printf("\n");
+    
+    student std;
+    fseek(mp, 0, SEEK_SET);
+    
+    if(fread(&std, sizeof(student), 1, mp)==1){
+        printf("Number: %d\n", std.student_no);
+        printf("Name: %s\n", std.name);
+        printf("Department: %s\n\n\n", std.department);
+    }
+    else{
+        printf(" The record is not found.\n\n");
+    }
+    
+}
 
-    int choice;
-    do {
-        printf("Menu:\n");
-        printf("1. Add Student\n");
-        printf("2. Search by Student Number\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                if (numStudents < MAX_STUDENTS) {
-                    printf("Enter student number: ");
-                    scanf("%d", &students[numStudents].studentNo);
-                    printf("Enter student name: ");
-                    scanf(" %[^\n]", students[numStudents].name);
-                    printf("Enter student department: ");
-                    scanf(" %[^\n]", students[numStudents].department);
-
-                    FILE *file = fopen("students.dat", "ab");
-                    if (file != NULL) {
-                        fwrite(&students[numStudents], sizeof(struct Student), 1, file);
-                        fclose(file);
-                        numStudents++;
-                    } else {
-                        printf("Error opening file.\n");
-                    }
-                } else {
-                    printf("Maximum number of students reached.\n");
-                }
-                break;
-            case 2:
-                if (numStudents == 0) {
-                    printf("No students in the database.\n");
-                } else {
-                    int searchNo;
-                    printf("Enter student number to search for: ");
-                    scanf("%d", &searchNo);
-
-                    FILE *file = fopen("students.dat", "rb");
-                    if (file != NULL) {
-                        int found = 0;
-                        struct Student student;
-                        while (fread(&student, sizeof(struct Student), 1, file) == 1) {
-                            if (student.studentNo == searchNo) {
-                                found = 1;
-                                printf("Student found:\n");
-                                printf("Student Number: %d\n", student.studentNo);
-                                printf("Name: %s\n", student.name);
-                                printf("Department: %s\n", student.department);
-                                break;
-                            }
-                        }
-                        if (!found) {
-                            printf("Student not found.\n");
-                        }
-                        fclose(file);
-                    } else {
-                        printf("Error opening file.\n");
-                    }
-                }
-                break;
-            case 3:
-                printf("Exiting the program.\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
+int main(){
+    int c;
+    FILE *mp = fopen("student.txt", "rb+");
+    
+    if(mp==NULL){
+        mp = fopen("student.txt", "wb+");
+        if(mp==NULL){
+            return 0;
         }
-    } while (choice != 3);
+    }
+    
+    while(1){
+        printf("Menu:\n");
+        printf("1.Adding a record.\n2.Search a record by his or her name.\n3.Exit.\n\n");
+        printf("Select your choice: ");
+        scanf("%d", &c);
+        printf("\n\n");
+        
+        switch(c){
+            case 1:
+                addStdRecord(mp);
+            break;
+            
+            case 2:
+                searchFileByName(mp);
+            break;
+            
+            case 3:
+                fclose(mp);
+                exit(0);
+            break;
+            
+            default:
+                printf(" Wrong. Try again...\n\n");
+            break;
+        }
+    }
 
     return 0;
 }
